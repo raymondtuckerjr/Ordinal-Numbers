ca92ae14f6471f00150cca735a12482158b3fca1
Raymond Tucker <45030878+raymondtuckerjr@users.noreply.github.com>
2019-05-09 08:30:28 -0400

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Ordinal Number</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Ordinal Numbers</h1>
+             <hr />
+
+            <p>
+                Cardinal number:
+                <asp:TextBox ID="cardinalNumber" runat="server" />
+                <asp:CompareValidator ID="numValidator" runat="server" type="Integer" ControlToValidate="cardinalNumber" Operator="DataTypeCheck">You must enter an integer greater than 0 </asp:CompareValidator><br /> Ordinal Number: <asp:Label ID="ordinalNumber" runat="server" />
+                </p>
+            <p>
+                <asp:Button ID="convert" runat="server" Text="Convert" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    if (Page.IsPostBack)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                int lastCharacters = 0;
+                if (cardinalNumber.Text.Length>1)
+                {
+                    lastCharacters = Convert.ToInt16(
+                        cardinalNumber.Text.Substring(
+                        cardinalNumber.Text.Length - 2));
+                    if (lastCharacters > 10 && lastCharacters < 14)
+                        ordinalNumber.Text = cardinalNumber.Text + "th";
+                    else
+                    {
+                        lastCharacters = Convert.ToInt16(
+                            cardinalNumber.Text.Substring(
+                             cardinalNumber.Text.Length - 1));
+                        if (lastCharacters == 1)
+                            ordinalNumber.Text = cardinalNumber.Text + "st";
+                        else if (lastCharacters == 2)
+                            ordinalNumber.Text = cardinalNumber.Text + "nd";
+                        else if (lastCharacters == 3)
+                            ordinalNumber.Text = cardinalNumber.Text + "rd";
+                        else
+                            ordinalNumber.Text = cardinalNumber.Text + "th";
+
+                    }
+                }
+                else if (cardinalNumber.Text.Length ==1)
+                {
+                    if (Convert.ToInt16(cardinalNumber.Text) == 1)
+                        ordinalNumber.Text = cardinalNumber.Text + "st";
+                    else if (Convert.ToInt16(cardinalNumber.Text) == 2)
+                        ordinalNumber.Text = cardinalNumber.Text + "nd";
+                    else if (Convert.ToInt16(
+                        cardinalNumber.Text) == 3)
+                        ordinalNumber.Text = cardinalNumber.Text + "rd";
+                    else
+                        ordinalNumber.Text = cardinalNumber.Text + "th";
+                }
+            }
+        }
+    }
+}
